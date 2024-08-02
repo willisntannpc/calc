4904e052e4b236b1ae7ae8126a27fd346be2e93f
willisntannpc <wn.mugoto@gmail.com>
2024-08-02 14:02:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+  const display = document.getElementById("display");
+
+  function appendToDisplay(input){
+    display.value += input;
+  }
+  
+function clearDisplay(){
+    display.value = "";
+}
+
+ function calculate(){
+    display.value = eval(display.value);
+}
+
+function calculate(){
+      display.value = eval(display.value);
+}
+
